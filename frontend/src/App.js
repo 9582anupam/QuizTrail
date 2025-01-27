d49ce6e2a7ade9a73a0e9adf5e68cd4f476a55af
Anupam <9582anupamk@gmail.com>
2025-01-28 01:26:12 +0530

    fix frontend deploy error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,24 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Navbar from "./components/pages/home/components/Navbar"
-// import Hero from "./components/pages/home/components/Hero"
-// import VideoSection from "./components/pages/home/components/VideoSection"
-// import TestimonialSection from "./components/pages/home/components/TestimonialSection"
-// import FAQSection from "./components/pages/home/components/FAQSection"
-// import ContactSection from "./components/pages/home/components/ContactSection"
-import Footer from "./components/pages/home/components/Footer"
-// import CTASection from "./components/pages/home/components/CTASection"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/pages/home/components/Navbar";
+import Footer from "./components/pages/home/components/Footer";
 import Home from "./components/pages/home/Home";
 
 function App() {
-  return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={ <Home /> 
-            }
-          />
-          {/* Add more routes here for other pages */}
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  )
+    return (
+        <Router>
+            <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+                <Navbar />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home />}
+                    />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App
